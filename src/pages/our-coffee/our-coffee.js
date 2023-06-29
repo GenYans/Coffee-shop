80b93b35e8d48ee70589bba76767ffc539b69ff6
GenYans <genapiter@yandex.ru>
2023-06-29 13:51:36 +0300

    Step 6.29.06.2023

--- src/pages/our-coffee/our-coffee.js
+++ src/pages/our-coffee/our-coffee.js
@@ -24,16 +24,19 @@ const OurCoffee = () => {
                 </section>
         
                 <section className='our-coffee-beans'>
-                    <img src={Girl} alt="girl" />
-                    <div className='our-coffee-beans-text'>
-                        <div className='our-coffee-beans-subheader'>About our beans</div>
-                        <img src={BorderLogoBlack} alt="BorderLogoWhite" />
-                        <div className='our-coffee-beans-descr'>Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible. 
-                        <br /><br />Afraid at highly months do things on at. Situation recommend objection <br />do intention so questions. 
-                        <br />As greatly removed calling pleased improve an. Last ask him cold feel <br />met spot shy want. 
-                        Children me laughing we prospect answered followed. At it went is <br />song that held help face.
+                    <div className='our-coffee-beans-wrapper'>
+                        <img src={Girl} alt="girl" />
+                        <div className='our-coffee-beans-text'>
+                            <div className='our-coffee-beans-subheader'>About our beans</div>
+                            <img src={BorderLogoBlack} alt="BorderLogoWhite" />
+                            <div className='our-coffee-beans-descr'>Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible. 
+                            <br /><br />Afraid at highly months do things on at. Situation recommend objection <br />do intention so questions. 
+                            <br />As greatly removed calling pleased improve an. Last ask him cold feel <br />met spot shy want. 
+                            Children me laughing we prospect answered followed. At it went is <br />song that held help face.
+                            </div>
                         </div>
                     </div>
+                    <div className='our-coffee-beans-line'></div>
             </section>
         </div> 
         </div>
